d450f33acecce40c7da23d3d333776f132c46d72
Yuki Watanabe <ukwhatn@gmail.com>
2024-04-16 16:14:05 +0900

    remove: dist dir

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Client = void 0;
-var client_1 = require("./module/client");
-Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return client_1.Client; } });
-//# sourceMappingURL=index.js.map
--- dist/module/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from './client';
-declare class SessionCreateException extends Error {
-}
-declare class HTTPAuthentication {
-    static login(client: Client, username: string, password: string): Promise<void>;
-    static logout(client: Client): Promise<void>;
-}
-export { HTTPAuthentication, SessionCreateException };
